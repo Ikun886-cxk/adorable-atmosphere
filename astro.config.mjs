de20f1347237b507b8a66e359e7c7aefe0c02e2a
DragonJay666 <liujinlisb@gmail.com>
2023-06-23 10:12:05 +0800

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,51 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'yaio小说',
+      defaultLocale: 'en',
+      locales: {
+        // English docs in `src/content/docs/en/`
+        en: {
+          label: 'English',
+        },
+        // Simplified Chinese docs in `src/content/docs/zh/`
+        zh: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+        ja: {
+          label: '日本語',
+          lang: 'ja'
+        },
+      },
       social: {
         github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
         {
-          label: 'Guides',
+          label: 'Father mating',
+          translations: {
+            zh: '亲父配种',
+            ja: '父親の交配',
+          },
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
+            { 
+              label: 'fat cock', 
+              link: '/breeding-father/fat-cock',
+              translations: {
+                zh: '肥公鸡',
+                ja: 'ふとくちんぽ',
+              }, 
+            },
+            {
+              label: 'father-son',
+              link: '/breeding-father/father-son',
+              translations: {
+                zh: '父子体检',
+                ja: '父子体检',
+              },
+            },
           ],
         },
         {
